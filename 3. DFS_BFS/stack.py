57d095a27a6aaca12e16c91835122d90d4d33bc0
n_heum <nakisara01@ajou.ac.kr>
2023-08-29 15:41:58 +0900

    DFS/BFS Concept Learning

---
+++ 3. DFS_BFS/stack.py
@@ -0,0 +1,15 @@
+# DFS/BFS 알고리즘 학습
+# Stack 개념 정리, 실습
+
+stack=[]
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+print(stack)
+print(stack[::-1])
